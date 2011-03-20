c4f840142c45dd3b2591025445db55227e19eaee
Patrick Paulin <patrick@modumind.com>
2011-03-20 09:09:58 -0700

    Simplifying lab 1. Supplying product project and method to test.

--- webapps/root/labs/lab-1/com.example.app.bootstrapper/src/com/example/app/bootstrapper/Activator.java
+++ webapps/root/labs/lab-1/com.example.app.bootstrapper/src/com/example/app/bootstrapper/Activator.java
@@ -68,4 +68,8 @@ public class Activator extends AbstractUIPlugin {
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
+	
+	String getMessage() {
+		return "Hello!";
+	}
 }
